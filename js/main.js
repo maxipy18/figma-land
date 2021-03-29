cf2d2af8a71ba44aabdd6189825748ad30829f5e
Maxim <maxipy@ya.ru>
2021-03-29 19:22:19 +0400

    add modal window

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const burger = document.querySelector('.burger-logo');
 const navMenu = document.querySelector('.nav-menu');
 const navMenuLink = document.querySelectorAll('.nav-menu__link');
 const headerNav = document.querySelector('.header-nav');
+const loginBtn = document.querySelector('.login-btn');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-btn');
 
 
 burger.addEventListener('click', ()=>{
@@ -13,4 +16,18 @@ burger.addEventListener('click', ()=>{
         item.classList.toggle('active');
     });   
     navMenu.style.top = `${headerNav.offsetHeight}px`;
+});
+
+loginBtn.addEventListener('click', ()=>{
+    modal.classList.toggle('active');    
+});
+
+modal.addEventListener('click', (event)=>{
+    if(event.target.classList.contains('modal')){
+        modal.classList.remove('active');
+    };
+});
+
+closeBtn.addEventListener('click', ()=>{
+    modal.classList.remove('active');
 });
